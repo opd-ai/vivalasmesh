b66913dcc35c4894e1e49370991d63fe539cac4c
GitHub Copilot <github-copilot-cli@github.com>
2026-08-09 22:15:07 -0400

    cleanup

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "Number of arguments: $#"
-echo "Arguments: $*"
--- test_import.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026 Viva Las Mesh Authors
-// SPDX-License-Identifier: MIT
-
-package main
-
-import _ "github.com/charmbracelet/bubbletea"
-
-func main() {}
